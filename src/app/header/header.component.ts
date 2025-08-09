0a1f5a8fcd3e234b566fd59aa21a8d6264f64533
julien54190 <lucadello.julien@hotmail.fr>
2025-08-09 18:06:24 +0200

    style(header): responsive + menu-burger ajout de fa-icon

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { HeaderMenuComponent } from './components/header-menu.component';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLinkActive, RouterLink],
+  imports: [RouterLinkActive, RouterLink, HeaderMenuComponent],
   template: `
     <header role="banner">
       <div class="flex align-items-center justify-content-between gap-16 px-12 py-6">
@@ -12,17 +13,16 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
           <span class="text-lg text-bold">QuizAréna</span>
         </a>
         <nav class="flex align-items-center gap-16 xs-hide">
-          <ul class="flex gap-12">
+          <ul class="flex align-items-center gap-12">
             <li><a routerLink="/accueil"  routerLinkActive="active-link">Accueil</a></li>
             <li><a routerLink="/jouer"  routerLinkActive="active-link">Jouer</a></li>
             <li><a routerLink="tableau-de-bord"  routerLinkActive="active-link">Mon espace</a></li>
             <li><a routerLink="/admin/tableau-de-bord"  routerLinkActive="active-link">Admin</a></li>
+            <li><a class="btn btn-outline-primary" routerLink="/connexion" routerLinkActive="active-link">Connexion</a></li>
+            <li><a class="btn btn-primary" routerLink="/inscription" routerLinkActive="active-link">Inscription</a></li>
           </ul>
         </nav>
-        <div class="flex gap-8">
-          <button class="btn btn-outline-primary">Connexion</button>
-          <button class="btn btn-primary">Inscription</button>
-        </div>
+        <app-header-menu class="hide xs-show"></app-header-menu>
       </div>
     </header>
   `,
