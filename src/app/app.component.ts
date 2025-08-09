84fdcaef57c45552313a3a0406712adac17a7c71
julien54190 <lucadello.julien@hotmail.fr>
2025-08-09 12:44:09 +0200

    initialisation du projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,8 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [],
+  template: ``
 })
 export class AppComponent {
   title = 'QuizArena';
