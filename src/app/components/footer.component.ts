3b3f789a25801de91f3ad97da911587146824c18
julien54190 <lucadello.julien@hotmail.fr>
2025-08-10 11:40:17 +0200

    style(footer): :lipstick: mise en place du style  + responsive

--- src/app/components/footer.component.ts
+++ src/app/components/footer.component.ts
@@ -4,32 +4,61 @@ import { Component } from '@angular/core';
   selector: 'app-footer',
   imports: [],
   template: `
-    <footer role="contentinfo">
+    <footer role="contentinfo" class="py-6">
       <div>
-        <div>
-          <div>
+        <div class="flex justify-content-between align-items-center gap-16 xs-flex-col">
+          <div class="flex align-items-center gap-8 p-12">
             <img src="assets/img/logo.png" alt="logo" class="logo">
-            <span>QuizAréna</span>
+            <span class="text-semibold">QuizAréna</span>
           </div>
-          <nav>
-            <ul>
-              <li><a>Acceuil</a></li>
-              <li><a>Contact</a></li>
-              <li><a>Confidentialité</a></li>
-              <li><a>Conditons</a></li>
-              <li><a>Cookies</a></li>
+          <nav aria-label="Liens utiles" class="flex justify-content-center">
+            <ul class="flex flex-wrap w-half gap-12 p-12 xs-w-full">
+              <li><a class="text-semibold">Accueil</a></li>
+              <li><a class="text-semibold">Confidentialité</a></li>
+              <li><a class="text-semibold">Contact</a></li>
+              <li><a class="text-semibold">Conditions</a></li>
+              <li><a class="text-semibold">Cookies</a></li>
             </ul>
           </nav>
-          <div>
-            <a title="Facebook"><i class="fa-brands fa-facebook" style="color: #74C0FC;"></i></a>
-            <a title="Instagram"><i class="fa-brands fa-square-instagram" style="color: #dd36a0;"></i></a>
+          <div class="flex align-items-center gap-8">
+            <a title="Facebook"><i class="fa-brands fa-facebook" style="color: #74C0FC; font-size: 32px;"></i></a>
+            <a title="Instagram"><i class="fa-brands fa-square-instagram" style="color: #dd36a0; font-size: 32px;"></i></a>
           </div>
         </div>
       </div>
-      <p>© 2024 QuizArena. Tous droits réservés</p>
+      <p class="text-center text-semibold">© 2024 QuizArena. Tous droits réservés</p>
     </footer>
   `,
-  styles: ``
+  styles: `
+    footer {
+      background-color: var(--primary);
+      color: var(--gray-100);
+      width: 100%;
+    }
+
+    span{
+      font-size: 1.5rem;
+    }
+
+    footer a {
+      color: var(--gray-100);
+    }
+
+    @media screen and (max-width: 920px) {
+      .logo {
+        width: 40px;
+        height: 40px;
+      }
+
+      span {
+        font-size: 1.2rem;
+      }
+
+      .fa-brands {
+        font-size: 24px !important;
+      }
+    }
+  `
 })
 export class FooterComponent {
 
