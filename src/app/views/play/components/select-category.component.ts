20b466a14039f389477be3834685d32f37084ecb
julien54190 <lucadello.julien@hotmail.fr>
2025-08-14 19:20:11 +0200

    style(play): :lipstick: ajout style pour les catégories

--- src/app/views/play/components/select-category.component.ts
+++ src/app/views/play/components/select-category.component.ts
@@ -6,24 +6,24 @@ import { ICategory } from '../../../interfaces/category';
   selector: 'app-select-category',
   imports: [],
   template: `
-    <section>
-      <header>
-        <h2>Choisissez une catégorie</h2>
-        <p>Sélectionnez le thème qui vous intéresse pour découvrir nos quiz</p>
-      </header>
-
-      <div>
-                @for (category of categories(); track category.id) {
-          <div (click)="selectCategory(category)">
-            <span>{{ category.icon }}</span>
-            <div>
-              <h3>{{ category.name }}</h3>
-              <p>{{ category.description }}</p>
-              <span>{{ category.quizCount }} quiz disponibles</span>
-            </div>
+    <section class="mt-20 card card-shadow">
+      <h2 class="text-center text-xlg">Choisissez une catégorie</h2>
+      <p class="text-sm text-center">Sélectionnez le thème qui vous intéresse pour découvrir nos quiz</p>
+      <div class="flex flex-wrap gap-16 filed w-full justify-content-center">
+              @for (category of categories(); track category.id) {
+        <div (click)="selectCategory(category)"
+        class="card card-white mt-10 card-size text-center flex flex-col justify-content-between card-hover">
+          <span>{{ category.icon }}</span>
+          <div>
+            <h3>{{ category.name }}</h3>
+            <p>{{ category.description }}</p>
+            <span class="badge-info">{{ category.quizCount }} quiz disponibles</span>
           </div>
-        }
+        </div>
+      }
       </div>
+
+
 
 
     </section>
